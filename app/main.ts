8d6432dacbf1439aa1b4d1f6fa5216cf7c0fcab5
Hristo Deshev <hristo@deshev.com>
2016-04-19 17:14:37 +0300

    Clean up main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,3 @@
-import * as application from "application";
-import * as frame from "ui/frame";
-
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {AppComponent} from "./app.component";
